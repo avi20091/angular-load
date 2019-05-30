6294713a0ff4140610c19376abb9018ec9b2436f
avi20091 <avi20091@gmail.com>
2019-05-30 12:37:54 +0530

    3 files added, 1 deleted

---
+++ src/app/countries.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CountriesService } from './countries.service';
+import { HttpErrorResponse } from '@angular/common/http';
+@Component({
+  selector: 'country',
+  templateUrl: './countries.component.html'
+})
+export class CountriesComponent{
+  private result:any;
+  private countriesSubscribe:any;
+  constructor(private _service:CountriesService){}
+  ngOnInit(){
+    this.countriesSubscribe=this._service.getCountries().subscribe(this._successCallBack,this._errorCallBack);
+  }
+  public _successCallBack=(res)=>{
+    this.result=res;
+  }
+  public _errorCallBack=(err:HttpErrorResponse)=>{
+    if(err.error instanceof Error){
+      console.log("Server side error...!!!");
+    }else{
+      console.log("Client Side Error...!!!")
+    }
+  }
+}
--- src/app/countries.service.ts
+++ src/app/countries.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Injectable()
-export class CustomerSrvice{
+export class CountriesService{
   constructor(private _http:HttpClient){};
-  public getCustomer():any{
+  public getCountries():any{
     this._http.get("https://restcountries.eu/rest/v2/all");
   }
 }
